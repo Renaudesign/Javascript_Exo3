129d2f6adffb7ac89a8a48cd9f0b2e45130a1b43
Renaud Sourmail <79084331+Renaudesign@users.noreply.github.com>
2021-03-08 14:07:22 +0100

    Update scriptJS.js

--- scriptJS.js
+++ scriptJS.js
@@ -1,8 +1,11 @@
 // @ts-check
-let subjectAge = Number(prompt("Veuillez indiquer vôtre âge, mon cher.", "99"))
-if (subjectAge >= 18){
+let subjectAge = Number(prompt("Veuillez indiquer vôtre âge, mon cher.", ""))
+if (subjectAge >= 18 && subjectAge < 100){
   alert("Vous êtes majeur")
 }
-else {
+else if (subjectAge < 18 && subjectAge > 0) {
   alert("Vous êtes mineur")
+}
+else {
+  alert("erreur de saisie")
 }
